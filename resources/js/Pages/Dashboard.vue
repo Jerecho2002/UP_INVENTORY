924449945b5d57d7b20a0d3c651960e453c32ff0
UncleDrew404 <leandrolabos456@gmail.com>
2025-10-15 11:19:30 +0800

    Added New Design

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -4,7 +4,7 @@ import { Head } from '@inertiajs/vue3';
 </script>
 
 <template>
-  <Head title="UP | Inventory" />
+  <Head title="UP | Dashboard" />
   <div class="bg-[#F3F3F3]">
     <DashboardLayout />
   </div>
