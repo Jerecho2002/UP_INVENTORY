dd6cb3cdaac6c39e72f74572129424072982c539
UncleDrew404 <leandrolabos456@gmail.com>
2025-11-14 10:58:42 +0800

    new updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,20 +15,19 @@ export default {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
-
             keyframes: {
-                fadeIn: {
-                    '0%': { opacity: '0', transform: 'translateY(20px)' },
-                    '100%': { opacity: '1', transform: 'translateY(0)' },
+                popIn: {
+                    '0%': { opacity: '0', transform: 'scale(0.7)' },
+                    '100%': { opacity: '1', transform: 'scale(1)' },
                 },
-                fadeOut: {
-                    '0%': { opacity: '1', transform: 'translateY(0)' },
-                    '100%': { opacity: '0', transform: 'translateY(20px)' },
+                popOut: {
+                    '0%': { opacity: '1', transform: 'scale(1)' },
+                    '100%': { opacity: '0', transform: 'scale(0.7)' },
                 },
             },
             animation: {
-                'fade-in': 'fadeIn 0.5s ease-in-out',
-                'fade-out': 'fadeOut 0.5s ease-in-out forwards',
+                'pop-in': 'popIn 0.5s ease-out',
+                'pop-out': 'popOut 0.5s ease-in forwards',
             },
         },
     },
