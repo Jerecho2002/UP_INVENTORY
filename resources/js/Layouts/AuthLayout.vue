32bdd023fb9a7944c1aca5b82715362fc3e85b50
UncleDrew404 <leandrolabos456@gmail.com>
2026-01-23 02:45:01 +0800

    New updates for Login And Register Design

--- resources/js/Layouts/AuthLayout.vue
+++ resources/js/Layouts/AuthLayout.vue
@@ -5,9 +5,24 @@ import { Link } from "@inertiajs/vue3";
 
 <template>
   <div class="flex flex-col md:flex-row min-h-screen">
-    <div class="w-full md:w-1/2"></div>
-    <div class="bg-[#850038] w-full md:w-1/2 flex items-center justify-center p-6 md:p-12" >
-      <div class="w-full sm:w-4/5 md:w-3/4 lg:w-2/3 xl:w-1/2 max-w-lg bg-white px-6 py-6 shadow-md rounded-lg">
+    <div class="w-full md:w-1/2">
+      <div class="flex flex-col items-center justify-center h-full p-6 md:p-12">
+        <Logo />
+        <h1 class="mt-6 text-3xl font-bold text-[#857F7F] text-center">
+          Welcome to UP Inventory
+        </h1>
+        <p class="mt-4 text-[#A6A4A4] text-center">
+          Manage your inventory efficiently and effectively with our
+          user-friendly platform.
+        </p>
+      </div>
+    </div>
+    <div
+      class="bg-[#850038] w-full md:w-1/2 flex items-center justify-center p-6 md:p-12"
+    >
+      <div
+        class="w-full sm:w-4/5 md:w-3/4 lg:w-2/3 xl:w-1/2 max-w-lg bg-white px-6 py-6 shadow-md rounded-lg"
+      >
         <slot />
       </div>
     </div>
--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -1,98 +1,111 @@
 <script setup>
-import Checkbox from '@/Components/Checkbox.vue';
-import AuthLayout from '@/Layouts/AuthLayout.vue';
-import InputError from '@/Components/InputError.vue';
-import InputLabel from '@/Components/InputLabel.vue';
-import TestButton from '@/Components/Buttons/TestButton.vue';
-import TextInput from '@/Components/TextInput.vue';
-import { Head, Link, useForm } from '@inertiajs/vue3';
+import Checkbox from "@/Components/Checkbox.vue";
+import AuthLayout from "@/Layouts/AuthLayout.vue";
+import InputError from "@/Components/InputError.vue";
+import InputLabel from "@/Components/InputLabel.vue";
+import TestButton from "@/Components/Buttons/TestButton.vue";
+import TextInput from "@/Components/TextInput.vue";
+import { Head, Link, useForm } from "@inertiajs/vue3";
 
 defineProps({
-    canResetPassword: {
-        type: Boolean,
-    },
-    status: {
-        type: String,
-    },
+  canResetPassword: {
+    type: Boolean,
+  },
+  status: {
+    type: String,
+  },
 });
 
 const form = useForm({
-    email: '',
-    password: '',
-    remember: false,
+  email: "",
+  password: "",
+  remember: false,
 });
 
 const submit = () => {
-    form.post(route('login'), {
-        onFinish: () => form.reset('password'),
-    });
+  form.post(route("login"), {
+    onFinish: () => form.reset("password"),
+  });
 };
 </script>
 
 <template>
-    <AuthLayout>
-        <Head title="Login" />
+  <AuthLayout>
+    <Head title="Login" />
 
-        <div v-if="status" class="mb-4 text-sm font-medium text-green-600">
-            {{ status }}
-        </div>
+    <div v-if="status" class="mb-4 text-sm font-medium text-green-600">
+      {{ status }}
+    </div>
 
-        <form @submit.prevent="submit">
-            <div>
-                <InputLabel for="email" value="Email" />
+    <form @submit.prevent="submit">
+      <div>
+        <InputLabel class="text-bold text-lg" for="email" value="Email" />
 
-                <TextInput
-                    id="email"
-                    type="email"
-                    placeholder="Enter your email"
-                    class="mt-1 block w-full"
-                    v-model="form.email"
-                    required
-                    autofocus
-                    autocomplete="username"
-                />
+        <TextInput
+          id="email"
+          type="email"
+          placeholder="Enter your email"
+          class="mt-1 block w-full py-3 text-md"
+          v-model="form.email"
+          required
+          autofocus
+          autocomplete="username"
+        />
 
-                <InputError class="mt-2" :message="form.errors.email" />
-            </div>
+        <InputError class="mt-2" :message="form.errors.email" />
+      </div>
 
-            <div class="mt-4">
-                <InputLabel for="password" value="Password" />
+      <div class="mt-4">
+        <InputLabel text-bold text-lg for="password" value="Password" />
 
-                <TextInput
-                    id="password"
-                    type="password"
-                    placeholder="Enter your password"
-                    class="mt-1 block w-full"
-                    v-model="form.password"
-                    required
-                    autocomplete="current-password"
-                />
+        <TextInput
+          id="password"
+          type="password"
+          placeholder="Enter your password"
+          class="mt-1 block w-full py-3 text-md"
+          v-model="form.password"
+          required
+          autocomplete="current-password"
+        />
 
-                <InputError class="mt-2" :message="form.errors.password" />
-            </div>
+        <InputError class="mt-2" :message="form.errors.password" />
+      </div>
 
-            <div class="mt-4 flex items-center justify-between">
-                <label class="flex items-center">
-                    <Checkbox name="remember" v-model:checked="form.remember" />
-                    <span class="ms-2 text-sm text-gray-600 dark:text-gray-400"
-                        >Remember me</span
-                    >
-                </label>
-                <Link class="rounded-md text-sm text-red-600 underline hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:text-gray-400 dark:hover:text-gray-100 dark:focus:ring-offset-gray-800"
-                >
-                    Forgot your password?
-                </Link>
-            </div>
+      <div class="mt-4 flex items-center justify-between">
+        <label class="flex items-center">
+          <Checkbox name="remember" v-model:checked="form.remember" />
+          <span class="ms-2 text-md text-gray-600 dark:text-gray-400"
+            >Remember me</span
+          >
+        </label>
+        <Link
+          class="rounded-md text-md text-[#850038] no-underline hover:underline focus:outline-none focus:ring-2 focus:ring-[#850038] focus:ring-offset-2"
+        >
+          Forgot your password?
+        </Link>
+      </div>
 
-            <div class="mt-4 flex items-center">
-                <TestButton
-                    class="ms-4"
-                    :class="{ 'opacity-25': form.processing }"
-                    :disabled="form.processing"
-                >
-                    Sign in
-                </TestButton>
-            </div>
-        </form>
-    </AuthLayout>
+      <div class="mt-12 flex items-center justify-center">
+        <TestButton
+          class="w-full justify-center text-lg font-bold py-3"
+          :class="{ 'opacity-25': form.processing }"
+          :disabled="form.processing"
+        >
+          Sign in
+        </TestButton>
+      </div>
+
+      <div>
+        <p class="mt-4 text-md items-center justify-center flex gap-2">
+          Don't have an account?
+          <Link
+            class="text-[#850038] no-underline hover:underline focus:outline-none focus:ring-2 focus:ring-[#850038] focus:ring-offset-2"
+            href="/register"
+          >
+            Register
+          </Link>
+        </p>
+      </div>
+    </form>
+  </AuthLayout>
 </template>
